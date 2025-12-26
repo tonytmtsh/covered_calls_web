4a76d6ac003714a4480f332f6572171c4ac7947b
tonytmtsh <tonytmtsh@users.noreply.github.com>
2025-12-26 17:16:00 +0000

    deploy: tonytmtsh/covered_calls@a8a1a1f58050b7d7d38422ef4df3e277fefaa335

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1972756249"
+    serviceWorkerVersion: "1448777949"
   }
 });
