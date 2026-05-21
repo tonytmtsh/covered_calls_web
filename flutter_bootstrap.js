ac45c14bf0b5911ba2f1d5c0688819ab43acecea
tonytmtsh <tonytmtsh@users.noreply.github.com>
2026-05-21 00:51:42 +0000

    deploy: tonytmtsh/covered_calls@cd02551135b72148b9c05d52aa9b55580937ad0d

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3085426223" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "780064536" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
